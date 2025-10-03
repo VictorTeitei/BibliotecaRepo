8c5e31d52889c4f313f5dcabe3da4c50aca636d3
VictorTeitei <victor75008@gmail.com>
2025-10-03 10:49:07 -0300

    ISSO AI

--- src/br/com/etec/biblioteca/cadastro/imagens/ClassePrincipal.java
+++ src/br/com/etec/biblioteca/cadastro/imagens/ClassePrincipal.java
@@ -3,8 +3,12 @@ package br.com.etec.biblioteca.cadastro.imagens;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ClasseInformacoes info_obj = new ClasseInformacoes();
+		ClasseInformacoes info_obj2 = new ClasseInformacoes("Dom Casmurro", "Machado de Assis")
 
+		info_obj.mostrarDados(); // null 0
+		info_obj.mostrarDados(); // null 0 autor e
 	}
 
 }
